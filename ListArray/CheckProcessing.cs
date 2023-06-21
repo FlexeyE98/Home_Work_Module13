44c89ae8254b967a0d0469e24bd23cf5aebaa9fb
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-06-21 16:52:14 +0300

    Итоговое задание модуля 13

---
+++ ListArray/CheckProcessing.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Encodings;
+
+namespace ListArray
+{
+    public static class CheckProcessing
+    {
+        readonly static System.Text.Encoding WINDOWS1251 = Encoding.GetEncoding(1251);
+        readonly static System.Text.Encoding UTF8 = Encoding.UTF8;
+
+        public static string ConvertWin1251ToUTF8(string inString)
+        {
+            return UTF8.GetString(WINDOWS1251.GetBytes(inString));
+        }
+
+        public static List<string> CheckTimeList(string text)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            var myList = new List<string>();
+
+            string temp = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (Char.IsPunctuation(text[i]) == false && Char.IsWhiteSpace(text[i]) == false)
+                {
+                    temp += text[i];
+                }
+
+                else
+                {
+                    myList.Add(temp);
+                    temp = "";
+                }
+            }
+            Console.WriteLine($"В коллекции: {myList.Count} слов");
+            Console.WriteLine($"Затраченное время List: {stopWatch.Elapsed.TotalSeconds}");
+            return myList;
+
+        }
+
+        public static LinkedList<string> CheckTimeLinkedList(string text)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            var myList = new LinkedList<string>();
+
+            string temp = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                if (Char.IsPunctuation(text[i]) == false && Char.IsWhiteSpace(text[i]) == false)
+                {
+                    temp += text[i];
+                }
+
+                else
+                {
+                    myList.AddFirst(temp);
+                    temp = "";
+                }
+            }
+            Console.WriteLine($"В коллекции: {myList.Count} слов");
+            Console.WriteLine($"Затраченное время LinkedList: {stopWatch.Elapsed.TotalSeconds}");
+            return myList;
+
+        }
+
+    }
+
+
+}
+
---
+++ ListArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Net;
+using static ListArray.Program;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+using System.Xml.Linq;
+using System;
+using System.Text;
+using System.Text.Unicode;
+
+namespace ListArray
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance); //Для кодировки из 1251 to UTF8
+            string path = @"C:\Users\Привет\Desktop\text.txt";
+            var read = File.ReadAllText(path);
+            var correctRead = CheckProcessing.ConvertWin1251ToUTF8(read);
+            var myList = CheckProcessing.CheckTimeList(correctRead); //(1)  Метод проверки выполнения затраченного времени коллекцией List<>
+            Console.WriteLine();
+            Console.WriteLine();
+            var myLinkedList = CheckProcessing.CheckTimeLinkedList(correctRead); //(2)   Метод проверки выполнения затраченного времени коллекцией LinkedList<>
+
+
+        }
+
+
+
+    
+
+    }
+
+
+
+
+}
+
