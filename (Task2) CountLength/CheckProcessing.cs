fb7f64e28d81cb6df59e307e4cfa0b2fa93f07c2
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-06-24 10:55:50 +0300

    Задание 2

---
+++ (Task2) CountLength/CheckProcessing.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Encodings;
+using static System.Net.Mime.MediaTypeNames;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+using System.Collections;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+namespace ListArray
+{
+
+    public static class CheckProcessing
+    {
+        readonly static System.Text.Encoding WINDOWS1251 = Encoding.GetEncoding(1251);
+        readonly static System.Text.Encoding UTF8 = Encoding.UTF8;
+
+
+
+        public static string ConvertWin1251ToUTF8(string inString)
+        {
+            return UTF8.GetString(WINDOWS1251.GetBytes(inString));
+        }
+
+        public static Dictionary<string,int> Counter(string text)
+        {
+            var myDictionary = new Dictionary<string, int>();
+            char[] separators = new char[] { ' ', '.', '-', ','};
+            string[] arrayText = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int counter = 1;
+
+
+            for (int i = 0; i < arrayText.Length; i++)
+            {
+                for (int j = i + 1; j < arrayText.Length; j++)
+                {
+                    if (arrayText[i] == arrayText[j])
+                    {
+                        counter++;
+                        if (!myDictionary.ContainsKey(arrayText[i]))
+                            myDictionary.Add(arrayText[i], counter);
+                        else
+                        {
+                            myDictionary[arrayText[i]]++;
+                        }
+                    }
+
+                    counter = 1;
+                }
+
+            }
+            var sortedDict = myDictionary.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            var result = new Dictionary<string, int>();
+            int counter2 = 0;
+
+            foreach (var i in sortedDict)
+            {
+                if (counter2 <= 10)
+                {
+                    result.Add(i.Key, i.Value);
+                    counter2++;
+                }
+
+            }
+            return result;
+        }
+
+    }
+}
+
+
+
+
---
+++ (Task2) CountLength/Program.cs
@@ -0,0 +1,23 @@
+﻿using ListArray;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _Task2__CountLength
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance); //Для кодировки из 1251 to UTF8
+            string path = @"C:\Users\Привет\Desktop\text.txt";
+            var read = File.ReadAllText(path);
+            var correctRead = CheckProcessing.ConvertWin1251ToUTF8(read);
+            var noPunctuationText = new string(correctRead.Where(c => !char.IsPunctuation(c)).ToArray());
+            var countWordsDict = CheckProcessing.Counter(noPunctuationText);
+
+            foreach(var i in countWordsDict)
+                Console.WriteLine($"Слово: {i.Key} встречается : {i.Value} раз");
+        }
+    }
+}
